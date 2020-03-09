1f51506bc4b1e689d3d68f6fccc07157bdf154a5
Mara <cizmas.mara@gmail.com>
2020-03-09 11:46:12 +0200

    tests added

--- src/test/java/lab2/AppTest.java
+++ src/test/java/lab2/AppTest.java
@@ -1,8 +1,11 @@
 package lab2;
 
-import static org.junit.Assert.assertTrue;
-
+import lab2.Domain.Student;
+import lab2.Repository.StudentRepo;
+import lab2.Validator.StudentValidator;
 import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 /**
  * Unit test for simple App.
@@ -16,5 +19,22 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+    @Test
+    public  void testAddStudent(){
+        StudentValidator studentValidator = new StudentValidator();
+        StudentRepo studentRepo = new StudentRepo(studentValidator,"C:\\Users\\Mara\\IdeaProjects\\lab2\\src\\main\\java\\lab2\\studenti.xml");
+        studentRepo.save(new Student("1","Ana",932,"email@email.com", "Prof. Dr. Ana"));
+        assertNotNull(studentRepo.findAll().iterator().hasNext());
+    }
+
+    @Test
+    public  void testAddStudent2(){
+        StudentValidator studentValidator = new StudentValidator();
+        StudentRepo studentRepo = new StudentRepo(studentValidator,"C:\\Users\\Mara\\IdeaProjects\\lab2\\src\\main\\java\\lab2\\studenti.xml");
+        studentRepo.save(new Student("1","Ana",932,"ce@a.com", "Prof. Dr. Ana"));
+        assertEquals("Ana",studentRepo.findOne("1").getNume());
+
     }
 }

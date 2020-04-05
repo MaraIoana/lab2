8ec6b588ba89b6cd3e0e693239a5e01a984d99d9
Mara <cizmas.mara@gmail.com>
2020-04-05 16:43:22 +0300

    lab3 in lab added

--- src/test/java/lab2/AppTest.java
+++ src/test/java/lab2/AppTest.java
@@ -1,8 +1,12 @@
 package lab2;
 
 import lab2.Domain.Student;
+import lab2.Domain.Teme;
 import lab2.Repository.StudentRepo;
+import lab2.Repository.TemeRepo;
 import lab2.Validator.StudentValidator;
+import lab2.Validator.TemeValidator;
+import lab2.Validator.ValidationException;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -36,5 +40,37 @@ public class AppTest
         studentRepo.save(new Student("1","Ana",932,"ce@a.com", "Prof. Dr. Ana"));
         assertEquals("Ana",studentRepo.findOne("1").getNume());
 
+    }
+
+    @Test
+    public void testAddAssignment1()
+    {
+        TemeValidator temeValidator = new TemeValidator();
+        TemeRepo temeRepo = new TemeRepo(temeValidator,"C:\\Users\\Mara\\IdeaProjects\\cat\\src\\main\\java\\lab2\\teme.xml");
+        try
+        {
+            temeRepo.save(new Teme(1,"aasa",1,2));
+            assertNotNull(temeRepo.findAll().iterator().hasNext());
+        }
+        catch(ValidationException e)
+        {
+            e.printStackTrace();
+            //assert("data not valid", e.getMessage());
+        }
+    }
+    @Test
+    public void testAddAssignment2()
+    {
+        TemeValidator temeValidator = new TemeValidator();
+        TemeRepo temeRepo = new TemeRepo(temeValidator,"C:\\Users\\Mara\\IdeaProjects\\cat\\src\\main\\java\\lab2\\teme.xml");
+        try
+        {
+            temeRepo.save(new Teme(1,"aasa",4,2));
+        }
+        catch(ValidationException e)
+        {
+            e.printStackTrace();
+            //assert("data not valid", e.getMessage());
+        }
     }
 }
